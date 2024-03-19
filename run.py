1cd6b2a0d449c4f45bcd935862a2b52e4acf8ec5
kosamad samad <KarenLSamad@gmail.com>
2024-03-19 13:48:20 +0000

    bug: run.py if name == retype to get pyhton3 run.py to work. Commented out code to section off inital set up in each file.

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ from taskmanager import app
 # checks the name class = main string. if yes then the app is running and we take the 3 arguments host, port and debug (stored in the environmant variable)
 if __name__ == "__main__":
     app.run(
-        host = os.environ.get("IP"),
-        PORT = int(os.environ.get("PORT")),
-        debug = os.environ.get("DEBUG")
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
     )
 
 #------------------------------------------------------------------------------
