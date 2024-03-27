6248708f5e340df0628c245b99ffc4190e2b5cfa
kosamad samad <KarenLSamad@gmail.com>
2024-03-27 10:46:15 +0000

    add runtime.txt file for deployment

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -16,7 +16,7 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABSE_URL")
     if uri.startswith("postgres://"):
          uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 # create instance of the sqlalchemy class above and assign to varible db set to instance of flass app (above)
 db = SQLAlchemy(app)
